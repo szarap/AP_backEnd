089ed5e08a2848f1a216e1b26d789b68e6248133
Pablo Gonzalez <lic.szgonzalez@gmail.com>
2022-07-19 13:28:34 -0300

    segundo mas crossorigin commit a nueva base de datos

--- Downloads/proyectoAP_SB/src/main/java/com/trabajoArgProg/proyectoAP_SB/controller/Controller.java
+++ Downloads/proyectoAP_SB/src/main/java/com/trabajoArgProg/proyectoAP_SB/controller/Controller.java
@@ -20,6 +20,7 @@ import com.trabajoArgProg.proyectoAP_SB.service.ITipoTrabajoService;
 import com.trabajoArgProg.proyectoAP_SB.service.IUserService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,6 +33,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@CrossOrigin(origins="https://porfolio-pablo-gonzalez.web.app")
 @RequestMapping("/api/")
 public class Controller {
     
