336f047598ea84682b0891efa2757e24a8ae9669
Pablo Gonzalez <lic.szgonzalez@gmail.com>
2022-07-19 13:54:34 -0300

    tercer mas agregar Cors config commit a nueva base de datos

--- Downloads/proyectoAP_SB/src/main/java/com/trabajoArgProg/proyectoAP_SB/ProyectoApSbApplication.java
+++ Downloads/proyectoAP_SB/src/main/java/com/trabajoArgProg/proyectoAP_SB/ProyectoApSbApplication.java
@@ -2,6 +2,9 @@ package com.trabajoArgProg.proyectoAP_SB;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ProyectoApSbApplication {
@@ -10,4 +13,18 @@ public class ProyectoApSbApplication {
 		SpringApplication.run(ProyectoApSbApplication.class, args);
 	}
 
+        
+        @Bean
+        public WebMvcConfigurer corsConfigurer(){
+            return new WebMvcConfigurer(){
+                @Override
+                public void addCorsMappings(CorsRegistry registry){
+                    registry.addMapping("/**").allowedOrigins("https://porfolio-pablo-gonzalez.web.app")
+                            .allowedMethods("*").allowedHeaders("*");
+                }
+            };
+        }
+        
+        
 }
+
